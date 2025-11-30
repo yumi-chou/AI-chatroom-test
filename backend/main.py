3c71fc067f44637a5b8594e7e93f01b2ee61676c
yumi <yumisunky@gmail.com>
2025-11-30 22:23:21 +0800

    deploy frontend4

--- backend/main.py
+++ backend/main.py
@@ -56,10 +56,7 @@ app = FastAPI(title="WK11 Chat API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5500",  
-        "https://ai-chatroom-test-jccg.vercel.app",
-    ],
+    allow_origins=["*"],         
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
